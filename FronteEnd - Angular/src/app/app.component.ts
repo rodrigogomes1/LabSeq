83effc08a82df4001895a5c68b1174b16244b27f
rodrigogomes1 <72028355+rodrigogomes1@users.noreply.github.com>
2023-05-11 13:08:23 +0100

    GUI Update - treat exceptions from backend

--- FronteEnd - Angular/src/app/app.component.ts
+++ FronteEnd - Angular/src/app/app.component.ts
@@ -8,24 +8,38 @@ import { BigNumber } from 'bignumber.js';
   //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  valueN?: number;
-  result?: string;
+  valueN?: number; //value passed in the input box of the UI
+  result?: string; //result of the labsec funtion showed in the UI
 
   constructor(private http: HttpClient) { }
 
+  /*
+  Funtion used to make the Rest request to the backend
+  The valueN can only be a number
+  The function catches the 400 bad request exceptions
+  from the backend.
+   */
   getResult(value: number | undefined) {
-    if (value!=undefined && value >0) {
-
+    if (value!=undefined) {
       fetch(`http://localhost:8080/labseq/${this.valueN}`)
-        .then(response => response.text())
+        .then(response => {
+          if (!response.ok) {
+            throw response.status
+          }
+          return response.text();
+        })
         .then(result => {
-          console.log(result); // "9062959782884117635"
           if (result !== undefined) {
             this.result = "The result is:"+ result;
           }
+        })
+        .catch(error => {
+          if (error == 400){
+            this.result = "Error: n value need to an Integer >= 0";
+          }
         });
     }else{
-      this.result = "The n value need to be selected. And it needs to be >0.";
+      this.result = "The n value need to be selected.";
     }
   }
 }
